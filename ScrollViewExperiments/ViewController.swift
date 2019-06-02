56bb25d162760f053a393e50192738e48348299b
Yuichi Fujiki <fjk89025@gmail.com>
2019-06-03 00:33:10 +0800

    Prepared UITableView.

--- ScrollViewExperiments/ViewController.swift
+++ ScrollViewExperiments/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    private var colors = [
+        UIColor.red,
+        UIColor.blue,
+        UIColor.yellow,
+        UIColor.black,
+        UIColor.green,
+        UIColor.gray,
+        UIColor.purple,
+        UIColor.orange,
+        UIColor.cyan,
+        UIColor.brown
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
+//    @objc func panGestureDetected(gestureRecognizer: UIPanGestureRecognizer) {
+//        let translation = gestureRecognizer.translation(in: scrollView)
+//        let velocity = gestureRecognizer.velocity(in: scrollView)
+//        NSLog("Pan gesture detected. Translation : \(translation), Velocity : \(velocity)")
+//    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88 // This is going to change for target cell
+    }
 }
 
